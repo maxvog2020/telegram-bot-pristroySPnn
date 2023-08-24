aa0a881b98ffccc713dc8512aa3af98497b6c071
Maxim Volgin <maxvog2020@gmail.com>
2023-08-24 23:46:11 +0400

    Update website

--- web/app.js
+++ web/app.js
@@ -1,13 +1,9 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+tg.enableClosingConfirmation()
 
 let main_button = tg.MainButton;
-main_button.text = "Главная кнопка";
+main_button.text = "Опубликовать";
 main_button.show();
 
-let back_button = tg.BackButton;
-back_button.show();
-
-document.getElementById("test").remove();
-console.log("Test");
