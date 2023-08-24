037f12321dcc6fd67df0771613daa8ae5cff53fe
Maxim Volgin <maxvog2020@gmail.com>
2023-08-24 22:18:26 +0400

    Update website

--- web/app.js
+++ web/app.js
@@ -6,5 +6,8 @@ let main_button = tg.MainButton;
 main_button.text = "Главная кнопка";
 main_button.show();
 
+let back_button = tg.BackButton;
+back_button.show();
+
 document.getElementById("test").remove();
 console.log("Test");
