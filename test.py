64cee7c9e7bdbb88e61fc227264f409f556dea6e
Maxim Volgin <maxvog2020@gmail.com>
2023-08-24 21:10:25 +0400

    Add inline button and update the website

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from config import config
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, WebAppInfo
 from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
 
 
 TOKEN = config.bot_token.get_secret_value()
@@ -15,13 +16,18 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(Command("start"))
 async def on_start(message: Message):
-    await message.answer(WEB)
+    markup = InlineKeyboardBuilder()
+    markup.add(InlineKeyboardButton(text="Кнопка!", web_app=WebAppInfo(url=WEB)))
+    await message.answer("Кликни на кнопку", reply_markup=markup.as_markup())
+
 
 #########################
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
